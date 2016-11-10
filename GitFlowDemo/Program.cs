54a44852941663c074be5f6249cd02f7166d1cf4
Padam Bhandari <Padam.Bhandari@datascan.com>
2016-11-10 09:55:15 -0600

    test after the git update

--- GitFlowDemo/Program.cs
+++ GitFlowDemo/Program.cs
@@ -10,6 +10,7 @@ namespace GitFlowDemo
     {
         static void Main(string[] args)
         {
+            //check after git update
 			//Hotfix 
             //first from gitflow1
             //fifth feature
